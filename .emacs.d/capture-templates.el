9cd2b2f9b424d34946a6f9ae5850b4724fd7821c
floppycode <floppycode@yahoo.de>
2013-01-17 21:51:59 +0100

    #Some more info ....

--- .emacs.d/capture-templates.el
+++ .emacs.d/capture-templates.el
@@ -1,6 +1,8 @@
 (setq org-capture-templates
  '(("t" "Todo" entry (file+headline "~/Zettelkasten/todo.org" "Todo-Eingang")
         "** TODO %?")
+   ("z" "Studium - Todo" entry (file+headline "~/Zettelkasten/todo.org" "Todo-Eingang-Studium")
+        "** TODO %?\n %a\n Entered on %U")
    ("j" "Journal" entry (file+datetree "~/org/journal.org")
         "* %?\nEntered on %U\n  %i\n  %a")
    ("w" "link template" entry
