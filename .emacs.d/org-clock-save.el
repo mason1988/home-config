5c05aa7520ddcab328743b597ade088b260c8ca1
Florian Lerch <floppycode@yahoo.de>
2013-01-25 18:00:11 +0100

    #Some more info ....

--- .emacs.d/org-clock-save.el
+++
@@ -1,2 +0,0 @@
-;; org-persist.el - waschmaschine at <2013-01-25 Fr 01:30>
-(setq stored-clock-history '(("/home/florian/Zettelkasten/refile.org" . 1) ("/home/florian/Zettelkasten/organisation.org" . 13)))
